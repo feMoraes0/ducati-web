c5b18b274a8c6f8892f8230972508c3290bf9c1e
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-04-11 09:32:06 -0300

    feat: add table data component

---
+++ src/components/TableData.vue
@@ -0,0 +1,57 @@
+<template>
+  <section>
+      <div v-for="item in data" :key="item.id" >
+        <list-title :text="item.title" />
+        <list-info :text="item.info" />
+      </div>
+  </section>
+</template>
+
+<script>
+import ListTitle from './ListTitle.vue';
+import ListInfo from './ListInfo.vue';
+
+export default {
+  name: 'TableData',
+  components: {
+    ListTitle,
+    ListInfo,
+  },
+  data() {
+    return {
+      data: [
+        {
+          id: 1,
+          title: 'Displacement',
+          info: '803cc',
+        },
+        {
+          id: 2,
+          title: 'Horse Power',
+          info: '73 hp (54 kW)',
+        },
+        {
+          id: 3,
+          title: 'Torque',
+          info: '67 Nm (49.0 lb-ft)',
+        },
+        {
+          id: 4,
+          title: 'Dry Weight',
+          info: '175 Kg (386 lb)',
+        },
+        {
+          id: 5,
+          title: 'Seat Height',
+          info: '805 mm (31.69 in)',
+        },
+        {
+          id: 6,
+          title: 'Safety',
+          info: 'ABS',
+        },
+      ],
+    };
+  },
+};
+</script>
