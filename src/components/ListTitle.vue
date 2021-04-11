7859d6a576c89d7f0edd75ef4d303ed833a06eae
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-04-10 21:18:00 -0300

    feat: add list title component

---
+++ src/components/ListTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>{{title}}</p>
+</template>
+
+<script>
+export default {
+  name: 'ListTitle',
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style lang="scss">
+p {
+  color: #181818;
+  font-size: 0.75rem;
+  font-weight: 300;
+  line-height: 0.875rem;
+}
+</style>
