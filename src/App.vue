70f82ca5591428f9574298bae3320b6078c045c9
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-04-10 21:36:20 -0300

    feat: add app component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,104 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<main>
+  <img class="menu" src="./assets/icons/menu.svg" alt="menu" />
+  <div class="logos">
+    <img class="ducati-logo" src="./assets/images/ducati-logo.png" alt="Ducati Logo" />
+    <img class="monster-logo" src="./assets/images/monster-logo.png" alt="Monster Logo" />
+    <h1>797</h1>
+  </div>
+  <div class="infos">
+    <div class="data">
+      <ListTitle text="Displacement" />
+      <ListInfo text="803cc" />
+      <ListTitle text="Horse Power" />
+      <ListInfo text="73 hp (54 kW)" />
+      <ListTitle text="Torque" />
+      <ListInfo text="67 Nm (49.0 lb-ft)" />
+      <ListTitle text="Dry Weight" />
+      <ListInfo text="175 Kg (386 lb)" />
+      <ListTitle text="Seat Height" />
+      <ListInfo text="805 mm (31.69 in)" />
+      <ListTitle text="Safety" />
+      <ListInfo text="ABS" />
+    </div>
+    <img src="./assets/images/slogan.png" alt="slogan">
+  </div>
+</main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import ListTitle from './components/ListTitle.vue';
+import ListInfo from './components/ListInfo.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld,
+    ListTitle,
+    ListInfo,
   },
+  name: 'App',
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;900&display=swap');
+
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+  font-family: "Lato", sans-serif;
+}
+
+main {
+  align-items: center;
+  background: url("./assets/images/background.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+  width: 100vw;
+
+  img.menu {
+    left: 9.875vw;
+    position: absolute;
+    top: 5.41vh;
+  }
+
+  div.logos {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+
+    img.ducati-logo {
+      margin-top: 5.08vh;
+    }
+
+    img.monster-logo {
+      margin-top: 3.75vh;
+    }
+
+    h1 {
+      color: white;
+      font-size: 12.5rem;
+      line-height: 15rem;
+      font-weight: 900;
+    }
+  }
+
+  div.infos {
+    align-items: flex-end;
+    background-color: white;
+    border-radius: 50px;
+    box-shadow: 0px 40px 30px rgba(60, 49, 13, 0.15);
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 7.33vh;
+    padding: 3.66vh 5.31vw 5.75vh 5vh;
+    width: 90vw;
+  }
 }
 </style>
