c73b6a813bea47c9b3ad8b1393d169e4310fd752
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-04-11 09:22:41 -0300

    feat: add colour selector component

---
+++ src/components/ColourSelector.vue
@@ -0,0 +1,53 @@
+<template>
+  <section>
+    <div
+      v-for="option in options"
+      class="colour-box"
+      :key="option.id"
+      :style="{ backgroundColor: option.colour }"
+    />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ColourSelector',
+  data() {
+    return {
+      options: [
+        {
+          id: 1,
+          colour: '#DF1F26',
+        },
+        {
+          id: 2,
+          colour: '#141414',
+        },
+        {
+          id: 3,
+          colour: '#E4E4E4',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  section {
+    display: flex;
+    flex-direction: row;
+
+    .colour-box {
+      border-radius: 16px;
+      cursor: pointer;
+      width: 6.6vh;
+      height: 6.6vh;
+      margin-left: 4.37vw;
+
+      &:first-child {
+        margin-left: 12.93vw;
+      }
+    }
+  }
+</style>
