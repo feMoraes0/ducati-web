b44d7766bbcef0ec8d2a82eb405ad96457340adb
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-04-10 21:31:39 -0300

    feat: add List Info component

---
+++ src/components/ListInfo.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>{{text}}</p>
+</template>
+
+<script>
+export default {
+  name: 'ListInfo',
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+p {
+  color: #181818;
+  font-size: 1.5rem;
+  font-weight: 900;
+  line-height: 1.875rem;
+  letter-spacing: 0.095rem;
+  margin-bottom: 0.687vh;
+}
+</style>
